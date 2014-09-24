196635ea4ddc86afafdbbb0d68f0ffda30c2412d
Juan Carlos Medina <juanca@mavenlink.com>
2014-09-23 22:26:30 -0700

    Initial commit: copy-pasta dialog-0.1.2; breaks, probably due to newer ruby; shameless fork

---
+++ dialog-0.1.2/lib/tailbox.rb
@@ -0,0 +1,20 @@
+require 'base'
+require 'textbox'
+
+module Dialog
+
+  # Displays a tailbox.
+  #
+  # Example:
+  #
+  #   Tailbox.new do |b|
+  #     b.text "Bla bla"
+  #   end
+  #
+  # Box option syntax:
+  #  --tailbox <file> <height> <width>
+  #
+  class Tailbox < Textbox
+  end
+
+end
---
+++ dialog-0.1.2/lib/tailboxbg.rb
@@ -0,0 +1,21 @@
+require 'base'
+require 'textbox'
+
+module Dialog
+
+  # Displays a tailbox in the background.
+  #
+  # Example:
+  #
+  #   Tailboxbg.new do |b|
+  #     b.text "Bla bla"
+  #     b.file "/var/log/messages"
+  #   end
+  #
+  # Box option syntax:
+  #  --tailboxbg <file> <height> <width>
+  #
+  class Tailboxbg < Textbox
+  end
+
+end
